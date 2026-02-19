548ea841682cd3811a3f4bad28facb092dce2220
MR7star <muthurupesh2007@gmail.com>
2026-02-19 15:29:07 +0530

    Update : added boundary colors, checking for errors.

--- dp.py
+++ dp.py
@@ -26,3 +26,47 @@ def simulate_move(graph, current_color, move_color):
                 new_colors[v] = move_color
                 queue.append(v)
     return new_colors
+
+def get_flooded_size(graph, colors):
+
+    """
+    Utility Function
+    Returns size of flooded region starting from node 0.
+    """
+    start_node = 0
+    target_color = colors[start_node]
+
+    queue = deque([start_node])
+    visited = set([start_node])
+
+    while queue:
+        node = queue.popleft()
+
+        for neighbour in graph[node]:
+            if neighbour not in visited and colors[neighbour] == target_color:
+                visited.add(neighbour)
+                queue.append(neighbour)
+    return len(visited)
+
+def boundary_colors(graph,colors):
+    start_node = 0
+    base_color = colors[start_node]
+
+    queue = deque([start_node])
+    visited = set([start_node])
+
+    while queue:
+        node = queue.popleft()
+        for neighbour in graph[node]:
+            if neighbour not in visited and colors[neighbour] == base_color:
+                visited.add(neighbour)
+                queue.append(neighbour)
+    
+    boundary = set()
+
+    for node in visited:
+        for neighbour in graph[node]:
+            if neighbour not in visited:
+                boundary.add(colors[neighbour])
+    
+    return boundary
